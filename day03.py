8ed1a839b7eba8e14fc5eddb756a67b8208d8861
Dennis Sauve <dengsauve@yahoo.com>
2025-12-02 22:10:21 -0800

    Day 3, Part 2 Complete

--- day03.py
+++ day03.py
@@ -1,9 +1,9 @@
 from input import day03
 
-# input = """987654321111111
-# 811111111111119
-# 234234234234278
-# 818181911112111""".split("\n")
+input = """987654321111111
+811111111111119
+234234234234278
+818181911112111""".split("\n")
 input = day03.split("\n")
 
 joltages = []
@@ -23,5 +23,33 @@ for bank in input:
     joltage = int(f"{highest_value}{second_highest_value}")
     joltages.append(joltage)
 
-print(joltages)
+# print(joltages)
 print(f"Part 1: {sum(joltages)}")
+
+# Part 2
+
+joltages = []
+
+for bank in input:
+    batteries = list(bank)
+    joltage = []
+    start_index = 0
+    end_index = len(batteries) - 11
+    for i in range(12):
+        # Find first highest number in array
+        highest_value = '0'
+        highest_value_index = 0
+        index_counter = 0
+        for battery in batteries[start_index:end_index]:
+            if battery > highest_value:
+                highest_value = battery
+                highest_value_index = index_counter
+            index_counter += 1
+        start_index = start_index + 1 + highest_value_index
+        end_index += 1
+        joltage.append(highest_value)
+    
+    joltages.append(int(''.join(joltage)))
+
+print(joltages)
+print(f"Part 2: {sum(joltages)}")
